44cd9e9d5626f860705570ae018e1548fb3c8d11
UKTQT <ufukkt2699@hotmail.com>
2022-06-24 17:04:41 +0300

    question 4 complete

--- assessment/html-css-js/question4.js
+++ assessment/html-css-js/question4.js
@@ -9,3 +9,15 @@
 // For 'Go Up and Down' button, it should go up touch the top of the viewport and return to middle in 1 second
 // Example is presented in question4.gif
 // Don't forget to add your css and javascript to index.html file
+
+
+// Hepsi tamam, fakat upDown fonksiyonunda div hareketini bitirdikten sonra 1 kere tıklayınca çalışmıyor, 2 kere tıklamak gerekli, muhtemelen sıfırlama gerekli, zaman bakımından üstüne düşmeyip diğerlerine geçmiş bulunmaktayım.
+const animatedDiv = document.querySelector('#to-be-animated');
+
+function rotate360() {
+    animatedDiv.classList.toggle("to-be-animated-rotate");
+}
+
+function upDown() {
+    animatedDiv.classList.toggle("to-be-animated-position");
+}
