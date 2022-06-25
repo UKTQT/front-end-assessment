e83bf1609f85d970e95b7cb848e04df2865f1c4c
UKTQT <ufukkt2699@hotmail.com>
2022-06-25 09:47:08 +0300

    question3 complete

--- assessment/html-css-js/question3.js
+++ assessment/html-css-js/question3.js
@@ -10,10 +10,12 @@
 
 // SOLUTION:
 
-// Bir timer, bir ms ve inputu idsi ile seçtiğimiz değişkenimiz var.
+// timer, ms ve inputu idsi ile dinlediğimiz değişkenlerimiz var.
 // inputun, karakter girildiği (keydown) ve karakter girilmesi bırakıldığı (keyup) anlarının tetiklenmelerini dinliyoruz.
-// Eğer inputa karakter girilip bırakıldıysa (keyup), o an timer değişkeni, bir setTimeOut başlatıyor ve herhangi başka bir giriş olasıya kadar ms işliyor ve sonunda fonksiyon çalışıyor.
-// Eğer setTimeOuta verilen ms devam ederken bir kere daha karakter girişi olursa, keydown tetiklenme dinlemesi çalışır ve timeri sıfırlar, bu sayede keyup tekrar çalışır ve üstteki işlemler devam eder.
+// Eğer inputa karakter girilip bırakıldıysa (keyup), timer sıfırlanır ve o an bir setTimeOut başlatılır.
+// 500 ms, giriş olmadığı sürece işler ve sonunda fonksiyon çalışır.
+// Eğer setTimeOuta verilen ms devam ederken yeni bir karakter girişi olursa,
+// keydown tetiklenme dinlemesi çalışır ve timeri sıfırlar, hemen ardından keyup tekrar çalışır ve üstteki işlemler devam eder.
 
 var timer;
 var ms = 500;
@@ -21,9 +23,7 @@ const inputVal = document.querySelector('#a_text');
 
 inputVal.addEventListener('keyup',function (e){
     clearTimeout(timer);
-    timer = setTimeout(() => {
-        console.log(e.target.value);
-    }, ms);
+    timer = setTimeout(() => {console.log(e.target.value);}, ms);
 });
 
 inputVal.addEventListener('keydown',function () {
